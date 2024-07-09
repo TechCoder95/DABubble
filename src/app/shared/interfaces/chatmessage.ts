503dd8a31d3bfe71f067517a95fdc9769ea2806c
Dominik Knezovic <dominik.knezovic@de.schunk.com>
2024-07-09 22:17:52 +0200

    npm updates

--- src/app/shared/interfaces/chatmessage.ts
+++ src/app/shared/interfaces/chatmessage.ts
@@ -1,23 +1,2 @@
 export interface Chatmessage {
-
-    id: number;
-    message: string;
-    sender: string;
-    time: string;
-    type: string;
-    status: string;
-    isMe?: boolean;
-    isRead?: boolean;
-    isDelivered: boolean;
-    isSent: boolean;
-    isPending: boolean;
-    isFailed: boolean;
-    isSeen: boolean;
-    isTyping: boolean;
-    isRecording: boolean;
-    isAudio: boolean;
-    isImage: boolean;
-    isVideo: boolean;
-   
-
 }
