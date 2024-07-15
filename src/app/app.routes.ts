78cb24ce04bbea57046cb47d32311858f541a8c4
RabiaUerkmez <111rabiaurkmez@gmail.com>
2024-07-15 23:19:12 +0200

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,15 @@
 import { Routes } from '@angular/router';
-<<<<<<< HEAD
 import { AddUserComponent } from './rabia/add-user/add-user.component';
 import { LoginComponent } from './knezovic/components/login/login.component';
 import { ChooseAvatarComponent } from './rabia/choose-avatar/choose-avatar.component';
+import { ChatComponent } from './Dimi/chat/chat.component';
 
 
 export const routes: Routes = [
     {path: '', component: AddUserComponent},
     {path: 'addUser', component: AddUserComponent},
     {path: 'avatar', component: ChooseAvatarComponent},
-    {path: 'login', component: LoginComponent}
+    {path: 'login', component: LoginComponent},
+    { path: 'chat', component: ChatComponent }
 ];
-=======
-import { ChatComponent } from './Dimi/chat/chat.component';
 
-export const routes: Routes = [{ path: '', component: ChatComponent }];
->>>>>>> origin/dimi
