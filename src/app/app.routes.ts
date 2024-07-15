77f23d182acd54baeb80f11cae07a933f5ab2db1
Dimitrios Kapetanis <dimitrioskapetanis1996@gmail.com>
2024-07-15 23:15:19 +0200

    merged

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,15 @@
 import { SidenavComponent } from './tristan/sidenav/sidenav.component';
-import { AddUserComponent } from './add-user/add-user.component';
 import { LoginComponent } from './knezovic/components/login/login.component';
-import { RegisterComponent } from './knezovic/components/register/register.component';
 import { Routes } from '@angular/router';
 import { ChatComponent } from './Dimi/chat/chat.component';
+import { ChooseAvatarComponent } from './rabia/choose-avatar/choose-avatar.component';
+import { AddUserComponent } from './rabia/add-user/add-user.component';
+
 
 export const routes: Routes = [
   { path: '', component: SidenavComponent },
   { path: 'add-user', component: AddUserComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
   { path: 'chat', component: ChatComponent },
   {path: 'avatar', component: ChooseAvatarComponent},
 ];
