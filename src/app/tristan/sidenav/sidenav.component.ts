21237f20a7e0657c34e271ceefbf5d26ab2fd1b9
Bladers123 <tristangehring123@hotmail.de>
2024-07-16 22:15:21 +0200

    colors in sidebar

--- src/app/tristan/sidenav/sidenav.component.ts
+++ src/app/tristan/sidenav/sidenav.component.ts
@@ -75,12 +75,11 @@ export class SidenavComponent implements OnInit {
   hasChild = (_: number, node: FlattenedNode) => node.expandable;
 
   isNewChannel(node: FlattenedNode): boolean {
-    return node.name !== 'Channel hinzufügen' && node.name.startsWith('#');
+    return !node.expandable && node.name !== 'Channel hinzufügen' && node.name !== 'Felix Müller' && node.name !== 'Noah Ewen';
   }
 
   async addChannel(channelName: string) {
-    const correctedChannelName = channelName.startsWith('#') ? channelName : `#${channelName}`;
-    const newChannel: TextChannel = { name: correctedChannelName };
+    const newChannel: TextChannel = { name: channelName };
     await this.dbService.addDataToDB('channels', newChannel);
     await this.loadChannels();
   }
