cc3f1c91e23ba00b930216cba45a1c83d873f6af
Rabia Ürkmez <111rabiaurkmez@gmail.com>
2024-07-20 08:52:02 +0200

    adding alert if register

--- src/app/rabia/choose-avatar/choose-avatar.component.ts
+++ src/app/rabia/choose-avatar/choose-avatar.component.ts
@@ -76,9 +76,17 @@ export class ChooseAvatarComponent {
     this.daStorage.uploadFile(file, localStorage.getItem("uId")!);
   }
 
+saveUser() {
+   this.registerUser = true;
+   console.log("sollte richtig sein", this.registerUser);
+   setTimeout(() => {
+    this.updateDatabase()
+   }, 2000);
+}  
+
 
   updateDatabase() {
-    this.registerUser = true;
+   
     this.UserService.updateUser(this.activeUser);
     this.registerUser = false;
     this.router.navigateByUrl('/home');
