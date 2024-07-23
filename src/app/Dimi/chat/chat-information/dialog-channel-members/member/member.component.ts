bc8380a27f427614ea0c2869156c259646e1abc3
Rabia Ürkmez <111rabiaurkmez@gmail.com>
2024-07-23 22:44:47 +0200

    small styles

--- src/app/Dimi/chat/chat-information/dialog-channel-members/member/member.component.ts
+++ src/app/Dimi/chat/chat-information/dialog-channel-members/member/member.component.ts
@@ -2,18 +2,21 @@ import { Component, Input } from '@angular/core';
 import { DialogChannelMembersComponent } from '../dialog-channel-members.component';
 import { OpenUserInfoComponent } from "../../../../../rabia/open-user-info/open-user-info.component";
 import { MatDialog } from '@angular/material/dialog';
+import { CommonModule } from '@angular/common';
+
 
 DialogChannelMembersComponent;
 
 @Component({
   selector: 'app-member',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './member.component.html',
   styleUrl: './member.component.scss',
 })
 export class MemberComponent {
   @Input() member: any;
+  online: boolean = false;
 
   constructor(public dialog: MatDialog) { }
 
