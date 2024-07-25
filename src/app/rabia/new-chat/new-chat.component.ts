b4046cd1f421b1aa623a9af2133e7e0ad83cc895
Rabia Ürkmez <111rabiaurkmez@gmail.com>
2024-07-25 11:39:56 +0200

    add new-chat-component

---
+++ src/app/rabia/new-chat/new-chat.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-chat',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-chat.component.html',
+  styleUrl: './new-chat.component.scss'
+})
+export class NewChatComponent {
+
+}
