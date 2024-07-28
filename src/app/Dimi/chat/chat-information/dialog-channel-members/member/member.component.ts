d41ee6429f1fd019392515fe9e4834e6dbb1e1a7
Dimitrios Kapetanis <dimitrioskapetanis1996@gmail.com>
2024-07-28 19:59:03 +0200

    refactor: Update member avatar property in member component template

--- src/app/Dimi/chat/chat-information/dialog-channel-members/member/member.component.ts
+++ src/app/Dimi/chat/chat-information/dialog-channel-members/member/member.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { DialogChannelMembersComponent } from '../dialog-channel-members.component';
-import { OpenUserInfoComponent } from "../../../../../rabia/open-user-info/open-user-info.component";
+import { OpenUserInfoComponent } from '../../../../../rabia/open-user-info/open-user-info.component';
 import { MatDialog } from '@angular/material/dialog';
 import { CommonModule } from '@angular/common';
-
+import { DABubbleUser } from '../../../../../shared/interfaces/user';
+import { UserService } from '../../../../../shared/services/user.service';
 
 DialogChannelMembersComponent;
 
@@ -15,17 +16,26 @@ DialogChannelMembersComponent;
   styleUrl: './member.component.scss',
 })
 export class MemberComponent {
+  activeUser!: DABubbleUser;
   @Input() member: any;
   online: boolean = false;
 
-  constructor(public dialog: MatDialog) { }
-
-  openInfo() {
-    console.log("das ist der: ", this.member);
-    this.dialog.open(OpenUserInfoComponent, {
-      data: { member: this.member }
-    });
+  constructor(public dialog: MatDialog, private userService: UserService) {
+    this.activeUser = this.userService.activeUser;
   }
 
+  getUserName() {
+    if (this.member === this.activeUser) {
+      return this.activeUser.username + ' (du)';
+    } else {
+      return this.member.username;
+    }
+  }
 
+  openInfo() {
+    console.log('das ist der: ', this.member);
+    this.dialog.open(OpenUserInfoComponent, {
+      data: { member: this.member },
+    });
+  }
 }
