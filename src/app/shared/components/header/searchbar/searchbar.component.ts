4515484e697ff6a1b6602cefb11a5455fc1df01d
Dominik Knezovic <dominik.knezovic@de.schunk.com>
2024-08-02 14:59:48 +0200

    User sind nun suchbar in home

--- src/app/shared/components/header/searchbar/searchbar.component.ts
+++ src/app/shared/components/header/searchbar/searchbar.component.ts
@@ -1,6 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { UserService } from '../../../services/user.service';
+import { ChannelService } from '../../../services/channel.service';
+import { ChatService } from '../../../services/chat.service';
 
 @Component({
   selector: 'app-searchbar',
@@ -14,10 +17,43 @@ export class SearchbarComponent {
   searchInput: string = '';
   searchResults: any[] = [];
 
-  constructor() { }
+  constructor(private userService:UserService, private channelService:ChannelService, private chatService : ChatService) { }
 
   search() {
     console.log('searching for: ' + this.searchInput);
+    this.searchResults = [];
+    this.userService.users.forEach(user => {
+      if (user.username?.includes(this.searchInput)) {
+        let searchItem = {
+          title: 'User: ',
+          description: user.username
+        }
+        this.searchResults.push(searchItem);
+      }
+    });
+
+    // Todo: Die Suche für die Channel und die Messages muss noch eingebaut werden!
+
+
+    // this.channelService.channel.forEach(channel => {
+    //   if (channel.name?.includes(this.searchInput)) {
+    //     let searchItem = {
+    //       title: channel.name,
+    //     }
+    //     this.searchResults.push(searchItem);
+    //   }
+    // });
+
+    // this.chatService..forEach(message => {
+    //   if (message.content?.includes(this.searchInput)) {
+    //     let searchItem = {
+    //       title: message.content,
+    //     }
+    //     this.searchResults.push(searchItem);
+    //   }
+    // }
+
+
   }
 
 
