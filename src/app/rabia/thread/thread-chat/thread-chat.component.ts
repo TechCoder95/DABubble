688848f4a7f8c6651591812314460da9e794bd55
Rabia Ürkmez <111rabiaurkmez@gmail.com>
2024-08-04 20:33:18 +0200

    fix bugs

--- src/app/rabia/thread/thread-chat/thread-chat.component.ts
+++ src/app/rabia/thread/thread-chat/thread-chat.component.ts
@@ -4,12 +4,13 @@ import { ThreadMyEmojiComponent } from "../thread-my-emoji/thread-my-emoji.compo
 import { TicketService } from '../../../shared/services/ticket.service';
 import { UserService } from '../../../shared/services/user.service';
 import { ChatMessage } from '../../../shared/interfaces/chatmessage';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-thread-chat',
   standalone: true,
-  imports: [ThreadEmojiComponent, ThreadMyEmojiComponent],
+  imports: [ThreadEmojiComponent, ThreadMyEmojiComponent, CommonModule],
   templateUrl: './thread-chat.component.html',
   styleUrl: './thread-chat.component.scss'
 })
@@ -17,10 +18,15 @@ export class ThreadChatComponent {
   @Input() receiveMessage!: ChatMessage;
   ticket: any;
 
-  constructor(public ticketService: TicketService) {
+  constructor(public ticketService: TicketService, private userService: UserService) {
     this.ticket = this.ticketService.getTicket();
 
     console.log("jiuiu", this.ticket);
+    this.getSenderAvatar();
+  }
 
+  getSenderAvatar() {
+    let user = this.userService.getOneUserbyId(this.ticket.senderId);
+    return user?.avatar;
   }
 }
