4c4b33e6793232dc8a86a272a83712ed60191e74
Rabia Ürkmez <111rabiaurkmez@gmail.com>
2024-08-06 09:41:14 +0200

    update test thread

--- src/app/rabia/thread/thread.component.ts
+++ src/app/rabia/thread/thread.component.ts
@@ -3,11 +3,14 @@ import { MatCardModule } from '@angular/material/card';
 import { ThreadReceiveChatComponent } from "./thread-receive-chat/thread-receive-chat.component";
 import { InputfieldComponent } from "../../Dimi/chat/chat-inputfield/inputfield.component";
 import { ChannelService } from '../../shared/services/channel.service';
-import { map, Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map, Observable, switchMap } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { UserService } from '../../shared/services/user.service';
 import { ThreadSendChatComponent } from "./thread-send-chat/thread-send-chat.component";
 import { MessageType } from '../../shared/components/enums/messagetype';
+import { FormControl } from '@angular/forms';
+import { DABubbleUser } from '../../shared/interfaces/user';
+import { TicketService } from '../../shared/services/ticket.service';
 
 @Component({
   selector: 'app-thread',
@@ -17,11 +20,35 @@ import { MessageType } from '../../shared/components/enums/messagetype';
   styleUrl: './thread.component.scss'
 })
 export class ThreadComponent {
-
+  searchControl = new FormControl();
+  searchResults: DABubbleUser[] = [];
+  searchQuery: string | undefined;
   showSingleThread!: boolean;
   messageType = MessageType.Threads;
+  selectedTicket: boolean = false;
 
-  constructor(public channelService: ChannelService, private userService: UserService) { }
+  constructor(public ticketService: TicketService, public channelService: ChannelService, private userService: UserService) { }
+
+  ngOnInit() {
+    this.searchControl.valueChanges.pipe(debounceTime(300), distinctUntilChanged(), switchMap(value => {
+      if (this.selectedTicket) {
+        this.selectedTicket = false;
+        return [];
+      }
+      return this.userService.searchUsersByNameOrEmail(value);
+    })
+    ).subscribe(results => {
+      this.searchResults = results;
+    });
+  }
+
+  selectUser(user: DABubbleUser) {
+    this.selectedTicket = true;
+    this.searchQuery = user.username;
+    this.searchControl.setValue(user.username);
+    this.searchResults = [];
+    this.userService.setSelectedUser(user);
+  }
 
   get getTitle(): Observable<string> {
     return this.channelService.selectedChannel$.pipe(
