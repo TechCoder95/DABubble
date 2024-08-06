80bcd079bcef3b4f1bf823a4f47faabdc5b4bd0d
Rabia Ürkmez <111rabiaurkmez@gmail.com>
2024-08-06 09:23:56 +0200

    Update thread.component.ts

--- src/app/rabia/thread/thread.component.ts
+++ src/app/rabia/thread/thread.component.ts
@@ -5,20 +5,21 @@ import { InputfieldComponent } from "../../Dimi/chat/chat-inputfield/inputfield.
 import { ChannelService } from '../../shared/services/channel.service';
 import { map, Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
-import { ThreadInputComponent } from "./thread-input/thread-input.component";
 import { UserService } from '../../shared/services/user.service';
 import { ThreadSendChatComponent } from "./thread-send-chat/thread-send-chat.component";
+import { MessageType } from '../../shared/components/enums/messagetype';
 
 @Component({
   selector: 'app-thread',
   standalone: true,
-  imports: [MatCardModule, ThreadReceiveChatComponent, InputfieldComponent, CommonModule, ThreadInputComponent, ThreadSendChatComponent],
+  imports: [MatCardModule, ThreadReceiveChatComponent, InputfieldComponent, CommonModule, ThreadSendChatComponent],
   templateUrl: './thread.component.html',
   styleUrl: './thread.component.scss'
 })
 export class ThreadComponent {
 
   showSingleThread!: boolean;
+  messageType = MessageType.Threads;
 
   constructor(public channelService: ChannelService, private userService: UserService) { }
 
