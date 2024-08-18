5d6360dfe011493a34ca97cd7fffb4fd9c1d1626
TechCoder95 <dominik.knezovic95@gmail.com>
2024-08-18 21:28:16 +0200

    chore: Refactor searchbar component and fix user avatar display

--- src/app/shared/components/header/searchbar/searchbar.component.ts
+++ src/app/shared/components/header/searchbar/searchbar.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { UserService } from '../../../services/user.service';
 import { DatabaseService } from '../../../services/database.service';
@@ -9,6 +9,14 @@ import { MatDialog } from '@angular/material/dialog';
 import { OpenUserInfoComponent } from '../../../../rabia/open-user-info/open-user-info.component';
 import { ChannelService } from '../../../services/channel.service';
 import { Router } from '@angular/router';
+import { DABubbleUser } from '../../../interfaces/user';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+
+interface SearchResult {
+  title: string;
+  description: any;
+}
 
 @Component({
   selector: 'app-searchbar',
@@ -17,89 +25,99 @@ import { Router } from '@angular/router';
   templateUrl: './searchbar.component.html',
   styleUrl: './searchbar.component.scss'
 })
-export class SearchbarComponent {
+export class SearchbarComponent implements OnInit {
+
+  private databaseService = inject(DatabaseService);
+  private userService = inject(UserService);
+  private channelService = inject(ChannelService);
+  private dialog = inject(MatDialog);
+  private router = inject(Router);
 
   searchInput: string = '';
-  searchResults: any[] = [];
-  channels: TextChannel[] = [];
-  messages: ChatMessage[] = [];
+  searchResults: SearchResult[] = [];
+  userSearch: DABubbleUser[] = [];
+  channelSearch: TextChannel[] = [];
+  messageSearch: ChatMessage[] = [];
 
+  private searchSubject: Subject<string> = new Subject();
 
-  constructor(private userService: UserService, private databaseService: DatabaseService, public dialog: MatDialog, private channelService: ChannelService, public router: Router) {
+  ngOnInit(): void {
+    this.searchSubject.pipe(
+      debounceTime(300) // 300ms debounce time
+    ).subscribe(searchText => {
+      this.searchInput = searchText;
+      this.searchItems();
+    });
 
-    this.channels = [];
-    this.messages = [];
+  }
 
-    // this.databaseService.getUserChannels(sessionStorage.getItem('userLogin')!).then((channels) => {
-    //   this.channels = channels;
-    //   channels.forEach(channel => {
-    //     this.databaseService.getMessagesByChannel(channel.id).then((messages) => {
-    //       this.messages = this.messages.concat(messages);
-    //     });
-    //   });
-    // });
+  onSearchInputChange(searchText: string) {
+    this.searchSubject.next(searchText);
   }
 
 
-  search() {
+
+  handleEnterKey(event: KeyboardEvent) {
+    if (event.key === 'Enter') {
+      this.onSearchInputChange(this.searchInput);
+    }
+  }
+
+
+
+  searchItems() {
     this.searchResults = [];
-    // this.pushUsers();
-    this.pushMessages();
+    this.pushUsers();
+    // this.pushMessages();
     this.pushChannels();
   }
 
-
-  // pushUsers() {
-  //   this.userService.users.forEach(user => {
-  //     if (user.username?.includes(this.searchInput)) {
-  //       let searchItem = {
-  //         title: 'User: ',
-  //         description: user.username,
-  //         photo: user.avatar
-  //       }
-  //       this.searchResults.push(searchItem);
-  //     }
-  //   });
-  // }
-
+  pushUsers() {
+    this.userService
+      .searchUsersByNameOrEmail(this.searchInput)
+      .then((results: DABubbleUser[]) => {
+        results.forEach(user => {
+          let searchItem = {
+            title: 'User: ',
+            description: user
+          }
+          this.searchResults.push(searchItem);
+        });
+      })
+      .catch((error) => {
+        console.error('Error fetching search results:', error);
+      });
+  }
 
   pushChannels() {
-    this.channels.forEach(channel => {
-      if (channel.name?.includes(this.searchInput)) {
-        let searchItem = {
-          title: 'Channel: ',
-          description: channel.name
-        }
-        this.searchResults.push(searchItem);
-      }
-    });
+    this.databaseService
+      .readDataByArray('channels', 'assignedUser', JSON.parse(sessionStorage.getItem('userLogin')!).id)
+      .then((results: TextChannel[]) => {
+        const newResults = results.filter(channel => channel.name.includes(this.searchInput));
+        newResults.forEach(channel => {
+          let searchItem = {
+            title: 'Channel: ',
+            description: channel
+          }
+          this.searchResults.push(searchItem);
+        });
+      }).catch((error) => {
+        console.error('Error fetching search results:', error);
+      });
   }
-
 
   pushMessages() {
-    this.messages.forEach(message => {
-      if (message.message?.includes(this.searchInput) && message.deleted === false) {
-        let searchItem = {
-          title: 'Message: ',
-          description: message.message,
-          channel: this.channels.find(channel => channel.id === message.channelId)?.name,
-          id: message.id
-        }
-        this.searchResults.push(searchItem);
-      }
-    });
-  }
 
+    //Todo Dome: Implement this
 
-  openProfile(profileUsername: string) {
-    // this.users.forEach(user => {
-    //   if (user.username === profileUsername) {
-    //     this.openInfo(user);
-    //   }
+    // let searchItem = {
+    //   title: 'Message: ',
+    //   description: message.message,
+    //   channel: this.channelSearch.find(channel => channel.id === message.channelId)?.name,
+    //   id: message.id
     // }
-    // );
+    // this.searchResults.push(searchItem);
   }
-
 
   openInfo(user: any) {
     this.dialog.open(OpenUserInfoComponent, {
@@ -109,21 +127,15 @@ export class SearchbarComponent {
     this.resetInput();
   }
 
-
-  openChannel(channelName: string) {
-    this.channels.forEach(channel => {
-      if (channel.name === channelName) {
-        this.channelService.selectChannel(channel);
-      }
-    });
+  openChannel(channel: TextChannel) {
+    this.channelService.selectChannel(channel);
+    this.router.navigate(['/home/channel', channel.id]);
     this.resetInput();
   }
 
-
   scrollToMessage(messageId: string) {
-    let message = this.messages.find(message => message.id === messageId);
+    let message = this.messageSearch.find(message => message.id === messageId);
     let x = message as unknown as ChatMessage;
-    this.openChannel(x.channelName!);
     if (x.id === messageId) {
       document.getElementById(x.id!)?.scrollIntoView()
       setTimeout(() => {
@@ -134,7 +146,6 @@ export class SearchbarComponent {
         this.resetInput();
       }, 2000);
     }
-
   }
 
   resetInput() {
--- src/app/shared/services/chat.service.ts
+++ src/app/shared/services/chat.service.ts
@@ -55,7 +55,6 @@ export class ChatService {
     const existingEmoji: any = this.getExistingEmoji(emoji);
     /* Überprüfen, ob der activeUser schon reagiert hat */
     if (this.userHasAlreadyReacted(emoji, existingEmoji)) {
-      debugger;
       this.eliminateUserReaction(existingEmoji, emoji);
     } else {
       this.addUserReaction(existingEmoji, emoji);
