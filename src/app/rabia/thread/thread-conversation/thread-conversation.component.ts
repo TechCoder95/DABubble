3ca3dab235fbbb0a96898b84a178a8ae3112d5a4
Rabia Ürkmez <111rabiaurkmez@gmail.com>
2024-08-25 10:50:21 +0200

    update

--- src/app/rabia/thread/thread-conversation/thread-conversation.component.ts
+++ src/app/rabia/thread/thread-conversation/thread-conversation.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { DABubbleUser } from '../../../shared/interfaces/user';
 import { ChatMessage } from '../../../shared/interfaces/chatmessage';
 import { CommonModule } from '@angular/common';
@@ -9,11 +9,12 @@ import { UserService } from '../../../shared/services/user.service';
 import { GlobalsubService } from '../../../shared/services/globalsub.service';
 import { ChannelService } from '../../../shared/services/channel.service';
 import { ReceiveChatMessageComponent } from "../../../Dimi/chat/chat-conversation/receive-chat-message/receive-chat-message.component";
+import { SendChatMessageComponent } from "../../../Dimi/chat/chat-conversation/send-chat-message/send-chat-message.component";
 
 @Component({
   selector: 'app-thread-conversation',
   standalone: true,
-  imports: [CommonModule, ReceiveChatMessageComponent],
+  imports: [CommonModule, ReceiveChatMessageComponent, SendChatMessageComponent],
   templateUrl: './thread-conversation.component.html',
   styleUrl: './thread-conversation.component.scss'
 })
@@ -24,7 +25,12 @@ export class ThreadConversationComponent {
   selectedThread!: TextChannel;
 
   @Output() receiveChatMessage = new EventEmitter<ChatMessage>();
+  @Output() sendChatMessage = new EventEmitter<ChatMessage>();
+
   @Input() activeUserFromChat: any;
+
+  @ViewChild('scrollContainer') scrollContainer!: ElementRef;
+  @ViewChildren('messageDay') messageDays!: QueryList<ElementRef>;
 
   constructor(private userService: UserService, private channelService: ChannelService, private databaseService: DatabaseService, private subService: GlobalsubService, public threadService: ThreadService) {
 
@@ -46,22 +52,20 @@ export class ThreadConversationComponent {
     // this.databaseService.subscribeToMessageDatainChannel(this.selectedThread.id);
     // this.allThreadMessages = [];
 
-    // this.databaseService.subscribeToMessageDatainChannel(this.selectedThread.id);
-    // this.allThreadMessages = [];
+    this.databaseService.subscribeToMessageDatainChannel(this.selectedThread.id);
+    this.allThreadMessages = [];
 
 
-
-
-    // this.subService.getAllMessageObservable().subscribe((message) => {
-    //   if (message.id) {
-    //     if (this.allThreadMessages.some((msg) => msg.id === message.id)) {
-    //       console.log(message, "das hier zeigt die nachricht an, die man grad eingegeben hat");
-    //       return;
-    //     }
-    //     this.allThreadMessages.push(message);
-    //     this.allThreadMessages.sort((a, b) => a.timestamp - b.timestamp);
-    //   }
-    // });
+    this.subService.getAllMessageObservable().subscribe((message) => {
+      if (message.id) {
+        if (this.allThreadMessages.some((msg) => msg.id === message.id)) {
+          console.log(message, "das hier zeigt die nachricht an, die man grad eingegeben hat");
+          return;
+        }
+        this.allThreadMessages.push(message);
+        this.allThreadMessages.sort((a, b) => a.timestamp - b.timestamp);
+      }
+    });
 
   }
 
@@ -95,4 +99,50 @@ export class ThreadConversationComponent {
     //   }
     // }
   }
+
+  // isSameDay(timestamp1: number, timestamp2: number): boolean {
+  //   let date1 = new Date(timestamp1);
+  //   let date2 = new Date(timestamp2);
+  //   return (
+  //     date1.getFullYear() === date2.getFullYear() &&
+  //     date1.getMonth() === date2.getMonth() &&
+  //     date1.getDate() === date2.getDate()
+  //   );
+  // }
+
+  // repeatedMessageInUnder5Minutes(
+  //   currentMessage: ChatMessage,
+  //   previousMessage: ChatMessage
+  // ): boolean {
+  //   let currentTime = new Date(currentMessage.timestamp).getTime();
+  //   let previousTime = new Date(previousMessage.timestamp).getTime();
+
+  //   let timeDifferenceInMinutes = (currentTime - previousTime) / (1000 * 60);
+
+  //   return timeDifferenceInMinutes < 10;
+  // }
+
+
+  // checkDate(timestamp: number): string {
+  //   let messageDate = new Date(timestamp);
+  //   return messageDate.toLocaleDateString();
+  // }
+
+
+  // formatDateWithDay(timestamp: any): string {
+  //   const messageDate = new Date(timestamp);
+  //   const options: Intl.DateTimeFormatOptions = {
+  //     weekday: 'long',
+  //     year: 'numeric',
+  //     month: 'long',
+  //     day: 'numeric',
+  //     timeZone: 'Europe/Berlin',
+  //   };
+  //   return messageDate.toLocaleDateString('de-DE', options);
+  // }
+
+  // scrollToBottom() {
+  //   this.scrollContainer.nativeElement.scrollTop =
+  //     this.scrollContainer.nativeElement.scrollHeight;
+  // }
 }
