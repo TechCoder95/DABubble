8ab3d14e8fef393889f3394aa0e5d152e34dd372
Dimitrios Kapetanis <dimitrioskapetanis1996@gmail.com>
2024-08-28 16:33:52 +0200

    refactor: Improve image download functionality in actual-receive-message component

--- src/app/Dimi/chat/chat-conversation/receive-chat-message/actual-receive-message/actual-receive-message.component.ts
+++ src/app/Dimi/chat/chat-conversation/receive-chat-message/actual-receive-message/actual-receive-message.component.ts
@@ -5,11 +5,14 @@ import { EmojisPipe } from '../../../../../shared/pipes/emojis.pipe';
 import { DAStorageService } from '../../../../../shared/services/dastorage.service';
 import express from 'express';
 import cors from 'cors';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogReceivedImageComponent } from './dialog-received-image/dialog-received-image.component';
+
 
 @Component({
   selector: 'app-actual-receive-message',
   standalone: true,
-  imports: [CommonModule, EmojisPipe],
+  imports: [CommonModule, EmojisPipe, DialogReceivedImageComponent],
   templateUrl: './actual-receive-message.component.html',
   styleUrl: './actual-receive-message.component.scss',
 })
@@ -18,7 +21,7 @@ export class ActualReceiveMessageComponent {
   @Input() receivedImgMessage!: string;
   @Input() repeatedMessage!: boolean | undefined;
 
-  constructor(private storage: DAStorageService) {}
+  constructor(public dialog: MatDialog) {}
 
   imageExists() {
     return (
@@ -26,61 +29,10 @@ export class ActualReceiveMessageComponent {
     );
   }
 
-  /* async downloadFile() {
-    await this.storage
-      .getDownloadURL(this.receiveMessage.fileUrl!)
-      .then((url) => {
-        debugger;
-        console.log(url);
-        
-        const xhr = new XMLHttpRequest();
-        xhr.responseType = 'blob';
-        xhr.onload = (event) => {
-          let blob = xhr.response;
-        };
-        xhr.open('GET', url);
-        xhr.send();
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  } */
-
-
-
-
-  async downloadFile() {
-
-    this.storage.downloadFile(this.receiveMessage.fileUrl!, this.receiveMessage.fileName!)
-
-
-    /* try {
-      const url = await this.storage.getDownloadURL(
-        this.receiveMessage.fileUrl!,
-      );
-      console.log(url);
-
-      const response = await fetch(url);
-      debugger;
-      console.log(response);
-
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      const blob = await response.blob();
-
-      const link = document.createElement('a');
-      const objectURL = URL.createObjectURL(blob);
-      link.href = objectURL;
-      link.download = this.receiveMessage.fileName || 'downloaded-file';
-      link.click();
-      URL.revokeObjectURL(objectURL); // Bereinige das Objekt-URL
-    } catch (error) {
-      console.error('Error downloading the file:', error);
-    } */
+    openImageInDialog() {
+    this.dialog.open(DialogReceivedImageComponent, {
+      data: { receivedImgMessage: this.receivedImgMessage }
+    });
   }
 }
-/* function cors(arg0: {}): any {
-  throw new Error('Function not implemented.');
-} */
 
